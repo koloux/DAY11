35c45ab2106d7c3c44dbd59e49e34c0885cb06a9
Huber Nicolas <nicolas@Hubers-MacBook-Pro.local>
2015-12-16 16:12:13 +0100

    NormTest

--- ex14/ft_list_sort.c
+++ ex14/ft_list_sort.c
@@ -6,30 +6,44 @@
 /*   By: nhuber <nhuber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 06:22:55 by nhuber            #+#    #+#             */
-/*   Updated: 2015/12/15 15:48:24 by nhuber           ###   ########.fr       */
+/*   Updated: 2015/12/16 15:36:52 by nhuber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
+void	ft_swap_elem(t_list **elem1, t_list **elem2)
+{
+	void	*tmp_data;
+
+	tmp_data = (*elem1)->data;
+	(*elem1)->data = (*elem2)->data;
+	(*elem2)->data = tmp_data;
+}
+
 void	ft_list_sort(t_list **begin_list, int (*cmp)())
 {
-	t_list	*elem;
-	t_list	*tmp;
+	t_list	*tmp1;
+	t_list	*tmp2;
+	int	sort;
 
-	elem = *begin_list;
-	if (*begin_list == NULL)
+	sort = 1;
+	if (*begin_list == NULL || (*begin_list)->next == NULL)
 		return ;
-	while (elem->next)
+	while (sort)
 	{
-		if (cmp(elem->data, elem->next->data) > 0)
+		sort = 0;
+		tmp1 = *begin_list;
+		tmp2 = tmp1->next;
+		while (tmp2)
 		{
-			tmp = elem;
-			elem = elem->next;
-			tmp->next = elem;
-			elem = *begin_list;
+			if (cmp(tmp1->data, tmp2->data) > 0)
+			{
+				ft_swap_elem(&tmp1, &tmp2);
+				sort = 1;
+			}
+			tmp1 = tmp1->next;
+			tmp2 = tmp1->next;
 		}
-		else
-			elem = elem->next;
 	}
 }
--- ex15/ft_list_reverse_fun.c
+++ ex15/ft_list_reverse_fun.c
@@ -6,7 +6,7 @@
 /*   By: nhuber <nhuber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 07:11:33 by nhuber            #+#    #+#             */
-/*   Updated: 2015/12/15 15:56:54 by nhuber           ###   ########.fr       */
+/*   Updated: 2015/12/16 16:11:40 by nhuber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 void	ft_list_reverse_fun(t_list *begin_list)
 {
-	t_list	*new_root;
 	t_list	*root;
 	t_list	*next_root;
+	t_list	*list;
 
-	new_root = begin_list;
-	next_root = NULL;
-	while (new_root)
+	root = NULL;
+	list = begin_list;
+	while (list)
 	{
-		root = new_root->next;
-		new_root->next = next_root;
-		next_root = new_root;
-		new_root = root;
+		next_root = list->next;
+		list->next = root;
+		root = list;
+		list = next_root;
 	}
-	begin_list = next_root;
+	begin_list = list;
 }
