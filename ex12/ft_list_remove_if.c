2d2751067ba181b3b66d37045d896eae0150d2b8
Huber Nicolas <nicolas@Hubers-MacBook-Pro.local>
2015-12-17 18:01:06 +0100

    trollgood

--- ex12/ft_list_remove_if.c
+++ ex12/ft_list_remove_if.c
@@ -6,18 +6,18 @@
 /*   By: nhuber <nhuber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 05:59:23 by nhuber            #+#    #+#             */
-/*   Updated: 2015/12/16 14:15:29 by nhuber           ###   ########.fr       */
+/*   Updated: 2015/12/17 18:00:55 by nhuber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+void	ft_list_remove_if(t_list *begin_list, void *data_ref, int (*cmp)())
 {
 	t_list	*tmp;
 	t_list	*victim;
 
-	tmp = *begin_list;
+	tmp = begin_list;
 	while (tmp)
 	{
 		if (cmp(tmp->data, data_ref) == 0)
