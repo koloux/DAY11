009292720ddad46a233081efb37ce5c7a62beaf5
Huber Nicolas <nicolas@Hubers-MacBook-Pro.local>
2015-12-28 12:07:02 +0100

    toPush

--- ex16/ft_sorted_list_insert.c
+++ ex16/ft_sorted_list_insert.c
@@ -6,7 +6,7 @@
 /*   By: nhuber <nhuber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 07:16:52 by nhuber            #+#    #+#             */
-/*   Updated: 2015/12/21 13:01:23 by nhuber           ###   ########.fr       */
+/*   Updated: 2015/12/21 17:03:31 by nhuber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,21 @@
 
 void	ft_sorted_list_insert(t_list **begin_list, void *data, int (*cmp)())
 {
-	t_list	*elem;
+	t_list	*list;
 	t_list	*new_elem;
+	t_list	*tmp;
 
-	elem = *begin_list;
-	
+	list = *begin_list;
+	new_elem = ft_create_elem(data);
+	if (list == NULL || cmp(list->data, data) > 0)
+	{
+		*begin_list = new_elem;
+		new_elem->next = list;
+		return ;
+	}
+	while (cmp(list->data, data) <= 0 && list->next)
+		list = list->next;
+	tmp = list->next;
+	list->next = new_elem;
+	new_elem->next = tmp;
 }
